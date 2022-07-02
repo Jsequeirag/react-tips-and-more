5f50961d3a98fa8c6d5b2ff307dc89f82591b7e8
Jose Sequeira <joseluissequeirag2@gmail.com>
2022-07-02 14:45:31 -0600

    tsparticle and canvas example

---
+++ stars-react-canvas/src/App.js
@@ -0,0 +1,15 @@
+import Home from "./components/Home/Home";
+import Section1 from "./components/Section1/Section1";
+import Section2 from "./components/Section2/Section2";
+import "./app.css";
+function App() {
+  return (
+    <>
+      <Home />
+      <Section1 />
+      <Section2 />
+    </>
+  );
+}
+
+export default App;
